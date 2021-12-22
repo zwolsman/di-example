9f8cae77127a5406a2e3294b5d18bbaa77a1b38c
Marvin Zwolsman <mzwolsman@bol.com>
2021-12-22 08:40:14 +0100

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Marvin Zwolsman on 22/12/2021.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/di_exampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  di_exampleApp.swift
+//  Shared
+//
+//  Created by Marvin Zwolsman on 22/12/2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct di_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
