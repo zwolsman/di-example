84368d828deb7639ac35a392f1bfca39ac37bffe
Marvin Zwolsman <mzwolsman@bol.com>
2022-01-13 21:16:24 +0100

    refactor: convert MoyaError -> Problem

--- Shared/Models/Problem.swift
+++ Shared/Models/Problem.swift
@@ -3,9 +3,41 @@
 //
 
 import Foundation
+import Combine
+import Moya
+import SwiftUI
 
-struct Problem: Codable, Error {
+struct Problem: Codable, Error, Identifiable {
+    var id: String {
+        title
+    }
     var title: String
-    var status: Int
+    var status: Int?
     var detail: String
+}
+
+extension Publisher where Failure == MoyaError {
+    func extractProblem() -> Publishers.MapError<Self, Problem> {
+        mapError { error in
+            guard let problem = try? error.response?.map(Problem.self) else {
+                return Problem.generic
+            }
+
+            return problem
+        }
+    }
+}
+
+extension Loadable {
+    var problem: Problem? {
+        error as? Problem
+    }
+}
+
+extension Problem {
+    static var generic: Problem = Problem(title: "Oops..", detail: "Something went wrong")
+
+    var alert: Alert {
+        Alert(title: Text(title), message: Text(detail))
+    }
 }
