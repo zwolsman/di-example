eb809b6f2cb86cfc962f53e39e907174937c3503
Marvin Zwolsman <mzwolsman@bol.com>
2022-05-01 12:07:25 +0200

    task: initial setup of event row

--- Shared/UI/Screens/Game/Components/EventRow.swift
+++ Shared/UI/Screens/Game/Components/EventRow.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct EventRow: View {
+    let tile: Tile
+    let tileId: Int
+    let gameColor: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            switch tile {
+            case .points(let amount):
+                Text("You found bits in tile \(tileId)!")
+                    .fontWeight(.semibold)
+                Text("+\(amount.formatted()) bits")
+                    .foregroundColor(.secondary)
+            case .bomb(revealedByUser: true):
+                Text("You hit a bomb in tile \(tileId)!")
+                    .fontWeight(.semibold)
+                Text("and lost \(10_000.formatted()) bits")
+                    .foregroundColor(.secondary)
+            default:
+                Text("TODO: \(String(describing: tile))")
+            }
+            
+        }
+        .padding(.leading, 12)
+        .overlay(ribbon, alignment: .leading)
+    }
+    
+    var ribbonColor: Color {
+        switch tile {
+        case .points(_):
+            return gameColor
+        case .bomb(_):
+            return Color("BombTileColor")
+        case .loading:
+            return .secondary
+        }
+    }
+    
+    var ribbon: some View {
+        RoundedRectangle(cornerRadius: 4, style: .continuous)
+            .foregroundColor(ribbonColor)
+            .frame(width: 4)
     }
 }
 
 struct EventRow_Previews: PreviewProvider {
     static var previews: some View {
-        EventRow()
+        EventRow(tile: .points(amount: 10_000), tileId: 17, gameColor: .red)
+            .previewLayout(.sizeThatFits)
+        
+        EventRow(tile: .bomb(revealedByUser: true), tileId: 11, gameColor: .red)
+            .previewLayout(.sizeThatFits)
+        
     }
 }
